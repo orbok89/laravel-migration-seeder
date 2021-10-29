0484aa1444567c2323b289becaf7c706109878d4
BigFelk <fedferrara89@gmail.com>
2021-10-29 14:17:48 +0200

    creata tabella

---
+++ database/migrations/2021_10_29_120730_create_trips_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTripsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->string('destination');
+            $table->smallInteger('prezzo');
+            $table->string('star_hotel');
+            $table->smallInteger('notti');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trips');
+    }
+}
